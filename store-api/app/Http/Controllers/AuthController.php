f2e2e842a9a32d794aa69213e16b9e9e0ddfb26e
siwat jermwatthana <siwat.j@hiveground.com>
2024-05-28 01:24:20 +0700

    Add darkaonline/l5-swagger dependency for API documentation

--- store-api/app/Http/Controllers/AuthController.php
+++ store-api/app/Http/Controllers/AuthController.php
@@ -8,6 +8,50 @@ use Illuminate\Support\Facades\Hash;
 
 class AuthController extends Controller
 {
+/**
+* @OA\Post(
+* path="/api/register",
+* operationId="Register",
+* tags={"Register"},
+* summary="User Register",
+* description="User Register here",
+*     @OA\RequestBody(
+*         @OA\JsonContent(),
+*         @OA\MediaType(
+*            mediaType="multipart/form-data",
+*            @OA\Schema(
+*               type="object",
+*               required={"fullname", "username", "email", "password", "password_confirmation", "tel", "role"},
+*               @OA\Property(property="fullname", type="text"),
+*               @OA\Property(property="username", type="text"),
+*               @OA\Property(property="email", type="text"),
+*               @OA\Property(property="password", type="password"),
+*               @OA\Property(property="tel", type="text"),
+*               @OA\Property(property="role", type="text"),
+*               @OA\Property(property="password_confirmation", type="password")
+*            ),
+*        ),
+*    ),
+*      @OA\Response(
+*          response=201,
+*          description="Register Successfully",
+*          @OA\JsonContent()
+*       ),
+*      @OA\Response(
+*          response=200,
+*          description="Register Successfully",
+*          @OA\JsonContent()
+*       ),
+*      @OA\Response(
+*          response=422,
+*          description="Unprocessable Entity",
+*          @OA\JsonContent()
+*       ),
+*      @OA\Response(response=400, description="Bad request"),
+*      @OA\Response(response=404, description="Resource Not Found"),
+* )
+*/
+
     // Register User
     public function register(Request $request) {
         // Validate field
--- store-api/app/Http/Controllers/Controller.php
+++ store-api/app/Http/Controllers/Controller.php
@@ -2,6 +2,12 @@
 
 namespace App\Http\Controllers;
 
+/**
+ * @OA\Info(
+ *    title="Store API",
+ *    version="1.0.0",
+ * )
+ */
 abstract class Controller
 {
     //
